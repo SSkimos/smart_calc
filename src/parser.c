3483aaf4ba96b525b66af751c6433488439c758e
Chereshnev <chereshnev.n.s@edu.mirea.ru>
2022-12-02 16:00:45 +0300

    :wrench: work on mvp parser

--- src/parser.c
+++ src/parser.c
@@ -14,36 +14,36 @@ char *parser(char *current_str) {
   struct s21_stack stack = make_stack();  // temp operand stack
   struct s21_stack ans = make_stack();  // final stack
 
-  char *buffer_string = NULL;
-  int buffer_value = 0;
-  int length = strlen(current_str);
-  char *ptr = &current_str[0];
+  int iterator = 0;
 
-//  for (int i = 0; i < length; i++) {
-//    if (*ptr - '0' >= 0 && *ptr - '0' <= 9) {
-//      int exp = 1;
-//      while (*ptr - '0' >= 0 && *ptr - '0' <= 9) {
-//        if (!buffer_string) {
-//          buffer_string = malloc(sizeof(char) * 2);
-//          buffer_string[0] = '\0';
-//          buffer_string[0] = *ptr;
-//          ptr++;
-//        } else {
-//          buffer_string = realloc(&buffer_string, (int)strlen(buffer_string) + 1);
-//          buffer_string[strlen(buffer_string)] = *ptr;
-//          ptr++;
-//        }
-//      }
-//      if (*ptr == '.') {
-//        ptr++;
-//        while (*ptr - '0' >= 0 && *ptr - '0' <= 9) {
-//         //
-//        }
-//      } else {
-//        push(&stack, buffer_value);
-//      }
-//    }
-//  }
+  // main iter func
+  for (int i = 0; i < strlen(current_str); i++) {
+    if (current_str[i] - '0' >= 0 && current_str[i] - '0' <= 9) {
+      //TODO: it is number
+    }
+    if (current_str[i] == '(') {
+      //TODO: it is bracket
+    }
+    if (current_str[i] == '+') {
+      //TODO: it is plus
+    }
+    if (current_str[i] == '-') {
+      //TODO: it is minus
+    }
+    if (current_str[i] == '*') {
+      //TODO: it is multiplication
+    }
+    if (current_str[i] == '/') {
+      //TODO: it is division
+    }
+    if (current_str[i] == '^') {
+      //TODO: it is degree
+    }
+    if (current_str[i] == '~') {
+      //TODO: it is unary minus
+    }
+  }
+
   // TODO: придумать коды ошибок
   return buffer_string;
 }
