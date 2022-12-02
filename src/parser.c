cf4229da57116cfabd225b8608fb536900be3870
Chereshnev <chereshnev.n.s@edu.mirea.ru>
2022-12-02 18:42:05 +0300

    :wrench: new feature: logging

--- src/parser.c
+++ src/parser.c
@@ -1,16 +1,14 @@
 #include "calc.h"
-#include <string.h>
-#include <stdlib.h>
 
 struct s21_stack *parser(struct s21_stack *output_stack, char *current_str) {
   struct s21_stack tmp_stack = make_stack();  // temp operand stack
-  printf(":debug: current case = '%s'\n", current_str);
-
   int iterator = 0;
+  FILE *logs;
+  logs = fopen("parser_logs.txt", "a");
 
   // main iter func
   for (int i = 0; i < strlen(current_str); i++) {
-    printf(":debug: current iterator = '%d'\n", i);
+    fprintf(logs, ":debug: current iterator = '%d'\n", i);
     if (current_str[i] - '0' >= 0 && current_str[i] - '0' <= 9) {
       //TODO: add parser float number
       char *number = NULL;
@@ -19,7 +17,7 @@ struct s21_stack *parser(struct s21_stack *output_stack, char *current_str) {
       while (current_str[i] - '0' >= 0 && current_str[i] - '0' <= 9) {
         number = realloc(number, strlen(number) + 1);
         number[(int)strlen(number)] = current_str[i];
-        printf(":debug: number = '%s'\n", number);
+        fprintf(logs, ":debug: number = '%s'\n", number);
         i++;
       }
       push(output_stack, number);
@@ -48,5 +46,6 @@ struct s21_stack *parser(struct s21_stack *output_stack, char *current_str) {
   }
 
   // TODO: придумать коды ошибок
+  fclose(logs);
   return output_stack;
 }
