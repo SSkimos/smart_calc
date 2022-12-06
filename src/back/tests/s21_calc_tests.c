a68d2cd6ac39cd5971dc9ef240f6cd36a0a6c403
Curt Rika <curtrika@su-d6.msk.21-school.ru>
2022-12-06 22:56:49 +0300

    :shirt: refactor

--- src/back/tests/s21_calc_tests.c
+++ src/back/tests/s21_calc_tests.c
@@ -1,114 +1,7 @@
 #include <check.h>
 #include "../calc.h"
 
-START_TEST(simple_stack_test) {
-  char *str = "123";
-  struct s21_stack stack = make_stack();
-  push(&stack, str);
-  char *str2 = "123124214124";
-  push(&stack, str2);
-  ck_assert_int_eq(0, strcmp(str2, pop(&stack)));
-  ck_assert_int_eq(0, strcmp(str, pop(&stack)));
-  clear_stack(&stack);
-} END_TEST
-
-START_TEST(stack_with_one_number) {
-  char *str1 = "22";
-  struct s21_stack stack1 = make_stack();
-  struct s21_stack *final_stack1;
-
-  final_stack1 = parser(&stack1, str1);
-  ck_assert_int_eq(0, strcmp(str1, pop(final_stack1)));
-  clear_stack(&stack1);
-
-  char *str2 = "2 2";
-  char *str = "2";
-  struct s21_stack stack2 = make_stack();
-  struct s21_stack *final_stack2;
-
-  final_stack2 = parser(&stack2, str2);
-  ck_assert_int_eq(0, strcmp(str, pop(final_stack2)));
-  ck_assert_int_eq(0, strcmp(str, pop(final_stack2)));
-} END_TEST
-
-START_TEST(simple_parser_test) {
-  char *str = "2 + 2";
-  char *str1 = "2";
-  char *str2 = "+";
-  struct s21_stack stack1 = make_stack();
-  parser(&stack1, str);
-  ck_assert_int_eq(0, strcmp(str2, pop(&stack1)));
-} END_TEST
-
-START_TEST(medium_parser_test) {
-  char *str = "3+4*2";
-  char *str1 = "+";
-  char *str2 = "*";
-  char *str3 = "2";
-  char *str4 = "4";
-  char *str5 = "3";
-  struct s21_stack stack1 = make_stack();
-  parser(&stack1, str);
-  ck_assert_int_eq(0, strcmp(str1, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str2, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str3, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str4, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str5, pop(&stack1)));
-} END_TEST
-
-START_TEST(medium_well_parser_test) {
-  char *str = "3+4*2/(123+200)";
-  char *str1 = "+";
-  char *str2 = "/";
-  char *str3 = "+";
-  char *str4 = "200";
-  char *str5 = "123";
-  char *str6 = "*";
-  char *str7 = "2";
-  char *str8 = "4";
-  char *str9 = "3";
-  struct s21_stack stack1 = make_stack();
-  parser(&stack1, str);
-  ck_assert_int_eq(0, strcmp(str1, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str2, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str3, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str4, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str5, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str6, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str7, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str8, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str9, pop(&stack1)));
-} END_TEST
-
-START_TEST(hard_parse_test) {
-  char *str = "(sin(2)+cos(3))^(400-1000/200)";
-  char *str1 = "^";
-  char *str2 = "-";
-  char *str3 = "/";
-  char *str4 = "200";
-  char *str5 = "1000";
-  char *str6 = "400";
-  char *str7 = "+";
-  char *str8 = "c";
-  char *str9 = "3";
-  char *str10 = "s";
-  char *str11 = "2";
-  struct s21_stack stack1 = make_stack();
-  parser(&stack1, str);
-  ck_assert_int_eq(0, strcmp(str1, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str2, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str3, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str4, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str5, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str6, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str7, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str8, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str9, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str10, pop(&stack1)));
-  ck_assert_int_eq(0, strcmp(str11, pop(&stack1)));
-} END_TEST
-
-START_TEST(first_full_back) {
+START_TEST(primitive_operations) {
   char *str1 = "2+2";
   long double ans = polish(str1, NULL);
   ck_assert_float_eq(4, ans);
@@ -124,35 +17,33 @@ START_TEST(first_full_back) {
   char *str4 = "1000/2";
   ans = polish(str4, NULL);
   ck_assert_float_eq(500, ans);
-}
 
-START_TEST(advanced_tests) {
-  char *str1 = "800/2*16+888";
-  long double ans = polish(str1, NULL);
+  char *str5 = "800/2*16+888";
+  ans = polish(str5, NULL);
   ck_assert_float_eq(7288, ans);
 
-  char *str2 = "200%2";
-  ans = polish(str2, NULL);
+  char *str6 = "200%2";
+  ans = polish(str6, NULL);
   ck_assert_float_eq(0, ans);
 
-  char *str3 = "201%2";
-  ans = polish(str3, NULL);
+  char *str7 = "201%2";
+  ans = polish(str7, NULL);
   ck_assert_float_eq(1, ans);
 
-  char *str4 = "2^2";
-  ans = polish(str4, NULL);
+  char *str8 = "2^2";
+  ans = polish(str8, NULL);
   ck_assert_float_eq(4, ans);
 
-  char *str5 = "sqrt(4)";
-  ans = polish(str5, NULL);
+  char *str9 = "sqrt(4)";
+  ans = polish(str9, NULL);
   ck_assert_float_eq(2, ans);
 
-  char *str6 = "log(10)";
-  ans = polish(str6, NULL);
+  char *str10 = "log(10)";
+  ans = polish(str10, NULL);
   ck_assert_float_eq(log(10), ans);
 
-  char *str7 = "ln(10)";
-  ans = polish(str7, NULL);
+  char *str11 = "ln(10)";
+  ans = polish(str11, NULL);
   ck_assert_float_eq(log10(10), ans);
 }
 
@@ -168,7 +59,7 @@ START_TEST(u_minus) {
   ck_assert_float_eq(-2, ans);
 }
 
-START_TEST(easy_trigonometry_tests) {
+START_TEST(primitive_trigonometry_tests) {
   char *str1 = "cos(1)";
   long double ans = polish(str1, NULL);
   ck_assert_double_eq_tol(cos(1), ans, 1e-7);
@@ -207,18 +98,11 @@ Suite *s21_calc_suite(void) {
   suite = suite_create("s21_calc");
   TCase *tcase_core = tcase_create("Core");
 
-  tcase_add_test(tcase_core, simple_stack_test);
-  tcase_add_test(tcase_core, stack_with_one_number);
-  tcase_add_test(tcase_core, simple_parser_test);
-  tcase_add_test(tcase_core, medium_parser_test);
-  tcase_add_test(tcase_core, medium_well_parser_test);
-  tcase_add_test(tcase_core, hard_parse_test);
-  tcase_add_test(tcase_core, first_full_back);
-  tcase_add_test(tcase_core, advanced_tests);
+  tcase_add_test(tcase_core, primitive_operations);
   tcase_add_test(tcase_core, float_tests);
   tcase_add_test(tcase_core, u_minus);
 
-  tcase_add_test(tcase_core, easy_trigonometry_tests);
+  tcase_add_test(tcase_core, primitive_trigonometry_tests);
 
   tcase_add_test(tcase_core, x_val);
 
